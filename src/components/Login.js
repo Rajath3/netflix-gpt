722ded58feb76139b44c6a829de5502c25d8b469
Rajath H R <rajath.nandan33@gmail.com>
2023-10-16 20:00:18 +0530

    Toggle form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import SignIn from './SignIn'
 
 const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const toggleSignIn = () => {
+    setIsSignIn(!isSignIn)
+  }
   return (
     <div>
       <Header/>
       <div className='absolute bg-blend-color-burn'>
         <img src='https://assets.nflxext.com/ffe/siteui/vlv3/ab180a27-b661-44d7-a6d9-940cb32f2f4a/7fb62e44-31fd-4e1f-b6ad-0b5c8c2a20ef/IN-en-20231009-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='logo'/>
       </div>
-      <SignIn/>
+      <SignIn isSignIn={isSignIn} toggleSignIn={toggleSignIn}/>
     </div>
   )
 }
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const SignIn = () => {
+const SignIn = ({isSignIn, toggleSignIn}) => {
+
+  const signInType = isSignIn ? 'Sign In': 'Sign Up'
+
   return (
     <div className='bg-black absolute mr-[30rem] ml-[34rem] mt-48 p-[4rem] bg-opacity-90 text-white '>
-        <h1 className='px-16 mb-8 text-4xl'>Sign In</h1>
+        <h1 className='px-16 mb-8 text-4xl'>{signInType}</h1>
         <form>
+            {!isSignIn && <input type='text' placeholder='Full Name' className='p-4 mx-16 my-2 w-8/12 bg-gray-500'/>}
             <input type='email' placeholder='Email Address' className='p-4 mx-16 my-2 w-8/12 bg-gray-500'/>
             <input type='password' placeholder='Password' className='p-4 mx-16 my-2 w-8/12 bg-gray-500'/>
-            <button className='bg-red-700 p-4 mx-16 mt-8 w-8/12'>Sign In</button>
+            <button className='bg-red-700 p-4 mx-16 mt-8 w-8/12'>{signInType}</button>
         </form>
+        <p className='text-white mx-16 mt-6' onClick={() => toggleSignIn()}>{isSignIn ? 'New to Netflix? Sign Up' : 'Registered User? Login'}</p>
     </div>
   )
 }
